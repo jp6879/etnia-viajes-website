001e9a8937773c10f4fb28dd2f719516ce98dc59
Juan Pablo Morales <juanpablom6789@gmail.com>
2026-02-01 10:21:06 -0300

    Fix async loading for maps api

--- src/lib/googleMaps.ts
+++ src/lib/googleMaps.ts
@@ -53,7 +53,7 @@ export function loadGoogleMapsScript(): Promise<void> {
 
     // Create and append script
     const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&loading=async`;
     script.async = true;
     script.defer = true;
     
